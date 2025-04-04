d2d66e1dbd2d68ba75bffddffe2e3538f94928f3
anaaakareem <anaaakareem@gmail.com>
2025-04-04 14:00:37 +0200

    Added more pages to be used later and created homepage file

--- scentSensation/store/views.py
+++ scentSensation/store/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Temporary')
+    return HttpResponse('Home Page')
+
+def signAccount(request):
+    return HttpResponse('Account Sign Page')
+
+def accountInformation(request):
+    return HttpResponse('Account Information')
+
+def store(request):
+    return HttpResponse('Store')
+
+def basket(request):
+    return HttpResponse('Basket Page')
