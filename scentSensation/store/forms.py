9de7c9d7ae1a64d6e971fb7575377592fc1f8a8b
anaaakareem <anaaakareem@gmail.com>
2025-04-11 17:27:21 +0200

    Commit.

---
+++ scentSensation/store/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    middle_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=True)
+    email_address = forms.EmailField(required=True)
+    DOB = forms.DateField(required=True, widget=forms.DateInput(attrs={'type': 'date'}))
+    phone_numbers = forms.CharField(max_length=15, required=False)
+    gender = forms.CharField(label= 'Gender', widget= forms.RadioSelect(choices=[('Male', 'Male'), ('Female', 'Female')]))
+    house = forms.CharField(max_length=25, required=True)
+    street_name = forms.CharField(max_length=100, required=True)
+    town_city = forms.CharField(max_length=50, required=True)
+    county = forms.CharField(max_length=50, required=True)
+    postcode = forms.CharField(max_length=10, required=True)
+    country = forms.ChoiceField(choices=[('England', 'England'), ('Scotland', 'Scotland')
+                                         ,('Wales', 'Wales'), ('Northern Ireland', 'Northern Ireland')], required=True)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+# class UserLoginForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#
+#     class Meta:
+#         model = User
+#         fields = ['email', 'password']
--- scentSensation/store/views.py
+++ scentSensation/store/views.py
@@ -1,11 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
+from django.contrib.auth.hashers import make_password
+from .forms import UserRegistrationForm
+import pymysql
 
 def home(request):
     return render(request, 'store/homepage.html')
 
 def signinout(request):
-    return render(request, 'store/account.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            # Example: Save to external DB using PyMySQL
+            connection = pymysql.connect(
+                host='localhost',
+                port=3306,
+                user='root',
+                password='Pass0-lap123',
+                db='fragrance_schema',
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+
+            try:
+                with connection.cursor() as cursor:
+                    sql = """
+                            INSERT INTO customer (
+                                first_name, middle_name, last_name, DOB, email_address, password, gender
+                            ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        """
+                    cursor.execute(sql, (
+                        data['first_name'], data.get('middle_name'), data['last_name'], data.get('DOB'),
+                        data['email_address'], make_password(data['password1']), data.get('gender')
+                    ))
+                    # Get customer_id
+                    customer_id = cursor.lastrowid
+                    # Get phone_numbers table
+                    phone_number = data.get('phone_number')
+                    if phone_number:
+                        phone_sql = "INSERT INTO phone_numbers (customer_id, phone_number) VALUES (%s, %s)"
+                        cursor.execute(phone_sql, (customer_id, phone_number))
+
+                        # Insert address
+                        house = data.get('house')
+                        street_name = data.get('street_name')
+                        town_city = data.get('town_city')
+                        county = data.get('county')
+                        postcode = data.get('postcode')
+                        country = data.get('country')
+
+                        if house and street_name and town_city and county and postcode and country:
+                            address_sql = """
+                                INSERT INTO addresses (
+                                    customer_id, house, street_name, town_city, county, postcode, country
+                                ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+                            """
+                            cursor.execute(address_sql, (
+                                customer_id, house, street_name, town_city, county, postcode, country
+                            ))
+                    connection.commit()
+                    messages.success(request, "User registered successfully!")
+                    return redirect(home)
+            finally:
+                connection.close()
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'store/account.html', {'form': form})
 
 def account(request):
     return render(request, 'store/accountInfo.html')
